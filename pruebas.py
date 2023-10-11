fb2fcef232f93b71b2a599564ca8601bc00e2b49
SIA <diegoamezaga@gmail.com>
2023-10-10 21:11:47 -0300

    comenzando a usar el decorador property

--- pruebas.py
+++ pruebas.py
@@ -5,3 +5,36 @@ def contenido():
     return "hola gandini"
 
 funcion(contenido())
+
+variable = contenido
+
+print(variable())
+
+def operacion(funcionnueva, saludo):
+    print("prueba")
+    funcion(saludo)
+
+operacion(funcion, contenido())
+
+"""funcion a recibe funcion b y retorna funcion c"""
+
+def funcion_a(funcion_b):
+    
+    def funcion_c(*args, **kwargs):
+        print("antes de ejecutar")
+        resultado = funcion_b(*args, **kwargs)
+        print("despues de la funcion")
+        
+        return resultado
+    
+    return funcion_c
+
+@funcion_a
+def hola():
+    print("hola desde funciòn saludar")
+
+@funcion_a
+def suma(a, b):
+    return a + b
+
+print(suma(20, 10))
