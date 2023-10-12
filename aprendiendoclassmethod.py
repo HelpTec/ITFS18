258b7cb4a27190f729c0ae8c8608edb7657cbcf6
SIA <diegoamezaga@gmail.com>
2023-10-12 19:43:10 -0300

    aprendiendo a usar class method

---
+++ aprendiendoclassmethod.py
@@ -0,0 +1,1 @@
+"""los class method son """
--- aprendiendodecoradores.py
+++ aprendiendodecoradores.py
@@ -26,9 +26,21 @@ class Celsius:
             raise ValueError("la temperatura por debajo de -237 no es posible")
         self.__temperatura = value
     
-objeto = Celsius(-10)
+objeto = Celsius()
+print(objeto.temperatura)
+objeto.temperatura = 10
+print(objeto.temperatura)
 print(objeto.afarenheit())
 
 """tuvimos varios errores basicos de identaciòn, hay que tenerlo en cuenta esto
 sin mencionar la falta de atenciòn a la hora de crear objetos de la clase Celsius llamandolos por
 nombres que no existian. A tener en cuenta en siguientes estudios"""
+
+"""en resumen, el decorador property se encarga de manejar los atributos de clases de una manera
+mas prolija y eficiente implementando la protecciòn de atributos de clases que python no tiene
+por su cuenta y otros lenguajes si.
+Sumado a que el decorador property ya tiene per se en sus argumentos las funciones get, set y del
+que terminan por ser llamadas y asignadas automaticamente por el programa al simplemente usar
+las funciones de objeto.atributo para el getter, objeto.atributo = valor para el setter y del,
+correspondientemente, sin necesidad de usar los parentesis ni crear metodos de codigo largo
+para manejar getters y setters que sin el propertys no funcionarian como se supone que lo harìan"""
