e2ab7fd8ec25c37b089f08998667c2b80fea70ab
SIA <diegoamezaga@gmail.com>
2023-10-11 21:28:42 -0300

    notas extra del estudio

--- aprendiendodecoradores.py
+++ aprendiendodecoradores.py
@@ -6,14 +6,17 @@ class Celsius:
     #inicializamos la clase
     def __init__(self, temperatura=0):
         self.temperatura = temperatura
+        
     #agregamos un metodo
     def afarenheit(self):
         return (self.temperatura * 1.8) + 32
+    
     #usamos el decorador property
     @property
     def temperatura(self):
         print("obteniendo temperatura...")
         return self.__temperatura
+    
     #ahora que decoramos al metodo temperatura con el property
     #declaramos un setter con un decorador para agregar el metodo
     @temperatura.setter
@@ -21,4 +24,11 @@ class Celsius:
         print("asignando valor... ")
         if value < -237.35:
             raise ValueError("la temperatura por debajo de -237 no es posible")
-        self.temperatura = value
+        self.__temperatura = value
+    
+objeto = Celsius(-10)
+print(objeto.afarenheit())
+
+"""tuvimos varios errores basicos de identaciòn, hay que tenerlo en cuenta esto
+sin mencionar la falta de atenciòn a la hora de crear objetos de la clase Celsius llamandolos por
+nombres que no existian. A tener en cuenta en siguientes estudios"""
