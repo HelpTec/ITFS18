1ae0d768a4d54690ef8215330f83b0b349e00c39
SIA <diegoamezaga@gmail.com>
2023-10-12 20:00:53 -0300

    metodos estaticos

--- aprendiendoclassmethod.py
+++ aprendiendoclassmethod.py
@@ -20,3 +20,18 @@ print(mi_clase.metododeclase())
 acceder a los metodos de la clase desde un objeto sin instanciar la misma y, aunque pueden modificar
 atributos de la clase no imagino como pueden hacerlo cuando de por medio usamos el property(o sin el
 como ya probè)"""
+
+"""un ejemplo de esto"""
+
+class Circulo:
+    
+    pi= 3.14
+    
+    @classmethod
+    def radio(cls, radio):
+        return cls.pi * (radio**2)
+
+resultado = Circulo.radio(2)
+print(resultado)
+
+"cabe destacar que un atributo de clase no pasa por un init, eso es algo bastante importante"
