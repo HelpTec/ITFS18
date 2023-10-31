b96c247176483be9b499ed7dc3d09318871d7511
SIA <diegoamezaga@gmail.com>
2023-10-31 20:46:54 -0300

    ejercicio de manejo de excepciones

--- abstractmethodsenpython.py
+++ abstractmethodsenpython.py
@@ -40,3 +40,39 @@ micontrol.bajar_Volumen()
 micontrol.canal_Anterior()
 micontrol.cambiar_Canal()
 micontrol.canal_Anterior()
+
+class Control_LG(Control_Remoto):
+
+    def subir_Volumen(self):
+        self.volumen += 1
+        print(self.volumen,"volumen arriba")
+    def bajar_Volumen(self):
+        self.volumen -= 1
+        print(self.volumen,"volumen abajo")
+    def cambiar_Canal(self):
+        self.canal += 1
+        print(self.canal,"canal siguiente")
+    def canal_Anterior(self):
+        if self.canal <= 1:
+            print(self.canal, "no se puede bajar mas")
+        else:
+            self.canal -= 1
+            print(self.canal,"canal anterior")
+
+micontrolLG = Control_LG()
+
+micontrolLG.subir_Volumen()
+micontrolLG.bajar_Volumen()
+micontrolLG.canal_Anterior()
+micontrolLG.cambiar_Canal()
+micontrolLG.canal_Anterior()
+
+"""en sì, las interfaces no cambian de como lo vimos en el sistema de control de capas, son definidas
+e implementadas por otra clase, en el sistema de capas las usamos para la capa controladora, aca
+por lo visto se usa para otra cosa
+en todo caso a esto se le conoce como INTERFACES INFORMALES"""
+
+"""implementar una interfaz como tal puede tener el problema de que no todas las clases implementen
+todos los metodos de la misma, ahì es donde entran las INTERFACES FORMALES. Las mismas obligan
+a todas las clases que implementan la interfaz a tambien implementar todos sus metodos
+esto se logra atraves de importar el modulo ABC"""
